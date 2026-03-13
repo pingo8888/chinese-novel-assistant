b5e5d061d296a688c1482bb2aadee385a405e8b7
pingo8888 <pingo8888@outlook.com>
2026-03-13 18:27:10 +0800

    refactor(core): reorganize and simplify code

--- src/services/vault-change-watcher.ts
+++ src/services/vault-change-watcher.ts
@@ -13,36 +13,20 @@ export function watchVaultChanges(
 	app: App,
 	onChange: (event: VaultChangeEvent) => void,
 ): () => void {
+	const emit = (type: VaultChangeType, file: TAbstractFile, oldPath?: string): void => {
+		onChange({
+			type,
+			file,
+			path: file.path,
+			oldPath,
+		});
+	};
+
 	const eventRefs = [
-		app.vault.on("create", (file) => {
-			onChange({
-				type: "create",
-				file,
-				path: file.path,
-			});
-		}),
-		app.vault.on("modify", (file) => {
-			onChange({
-				type: "modify",
-				file,
-				path: file.path,
-			});
-		}),
-		app.vault.on("delete", (file) => {
-			onChange({
-				type: "delete",
-				file,
-				path: file.path,
-			});
-		}),
-		app.vault.on("rename", (file, oldPath) => {
-			onChange({
-				type: "rename",
-				file,
-				path: file.path,
-				oldPath,
-			});
-		}),
+		app.vault.on("create", (file) => emit("create", file)),
+		app.vault.on("modify", (file) => emit("modify", file)),
+		app.vault.on("delete", (file) => emit("delete", file)),
+		app.vault.on("rename", (file, oldPath) => emit("rename", file, oldPath)),
 	];
 
 	return () => {
